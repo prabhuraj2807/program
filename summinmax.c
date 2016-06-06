3065c522e8f2afadbc78a81e60bc4319d6054ca6
prabhuraj2807 <prabhukceitb13@gmail.com>
2016-06-07 00:58:51 +0530

    Create summinmax.c

---
+++ summinmax.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int cmp(const int *a,const int *b){
+	return *(int *)a-*(int *)b;
+}
+int main(void) {
+	int testcase;
+	scanf("%d",&testcase);
+	while(testcase--){
+		int no;//number of elements
+		scanf("%d",&no);//read no of elements
+		int loop,array[no];
+		for(loop=0;loop<no;loop++){
+			scanf("%d",&array[loop]);
+		}
+		qsort(array,no,sizeof(int),cmp);
+		
+		printf("Sum of Minimum & Maximum element in array : %d",array[0]+array[no-1]);
+	}
+	return 0;
+}
